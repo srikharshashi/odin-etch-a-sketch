f3a5f8f58cb30608b18194da3eb2e43c42deff01
Srikhar Shashi D <dasojusrikhar@gmail.com>
2021-12-23 23:40:31 +0530

    base functionality 3

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const maincont = document.querySelector(".container");
 const start = document.querySelector(".startbutton");
 const reset = document.querySelector(".reset");
 const slider = document.querySelector(".slider");
-const sqno=document.querySelector(".sqno");
-console.log(slider);
+const sqno = document.querySelector(".sqno");
 maincont.style.height = "700px";
 maincont.style.width = "700px";
 var squares = 20;
 var side = 700 / squares;
 var _isactive = false;
 var inner = undefined;
+var color="aqua";
 
 
 function init() {
@@ -28,33 +28,12 @@ function init() {
          newbo.classList.add(`data-key=${j}`);
       }
    }
-   inner=document.querySelectorAll(".inner");
-   sqno.textContent=`${squares}`;
+   inner = document.querySelectorAll(".inner");
+   sqno.textContent = `Sqaures  :  ${squares}`;
 }
 init();
 
-
-
-
 //Reset button
-
-
-const resetf = () => {
-   console.log("click on reset");
-   _isactive = false;
-   start.textContent = "Start";
-   fun2();
-   inner.forEach((e) => {
-      e.classList.remove("colored");
-   })
-};
-
-reset.onclick = resetf;
-
-const myfunc = function (block) {
-   this.classList.add("colored");
-}
-
 const fun2 = function () {
    start.textContent = "Start";
    inner.forEach((inn) => {
@@ -62,11 +41,28 @@ const fun2 = function () {
    });
    _isactive = false;
 }
+
+const resetf = () => {
+   _isactive = false;
+   start.textContent = "Start";
+   fun2();
+   inner.forEach((e) => {
+      e.style.backgroundColor="white";
+   })
+};
+
+reset.onclick = resetf;
+
+const myfunc = function (block) {
+   this.style.backgroundColor=color;
+}
+
+
 start.onclick = () => {
    if (!_isactive)
    //if not active then start
    {
-      console.log(inner);
+      // console.log(inner);
 
       start.textContent = "Pause";
       _isactive = true;
@@ -85,9 +81,56 @@ slider.oninput = () => {
    squares = slider.value;
    resetf();//removes the color class
    _isactive = false;//sets the active status to false
-   const allouter=document.querySelectorAll(".outer");
-   allouter.forEach((each)=>{
+   const allouter = document.querySelectorAll(".outer");
+   allouter.forEach((each) => {
       each.remove();
    });
    init();
 }
+
+const colorselect = document.querySelector(".colorselect");
+colorselect.onclick = (event) => {
+   const inn=document.querySelectorAll(".inner");
+   switch (colorselect.value) {
+      case "red":
+         {
+            inn.forEach((each)=>{
+               if(each.style.backgroundColor==color)
+               {
+                  each.style.backgroundColor="red";
+               }
+            });
+            color="red";
+
+         }
+         break;
+         case "green":
+         {
+            inn.forEach((each)=>{
+               if(each.style.backgroundColor==color)
+               {
+                  each.style.backgroundColor="green";
+               }
+            });
+            color="green";
+
+         }
+         break;
+         case "aqua":
+         {
+            inn.forEach((each)=>{
+               if(each.style.backgroundColor==color)
+               {
+                  each.style.backgroundColor="aqua";
+               }
+            });
+            color="aqua";
+
+         }
+         break;
+   
+      default:
+         break;
+      
+   }
+};
